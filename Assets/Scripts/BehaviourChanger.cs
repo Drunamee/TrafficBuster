b25a64c5241d885662066d07616669df985efa48
Drunamee <drunamedruname@gmail.com>
2021-11-02 18:38:59 +0300

    Druname

---
+++ Assets/Scripts/BehaviourChanger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviourChanger : MonoBehaviour
+{
+    public BCG_EnterExitVehicle Vehicle;
+    public int BehaviourType;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void OnEnable()
+    {
+        BCG_EnterExitPlayer.OnBCGPlayerEnteredAVehicle += OnSitInCar;
+    }
+    void OnSitInCar(BCG_EnterExitPlayer player, BCG_EnterExitVehicle vehicle)
+    {
+        if(vehicle == this.Vehicle)
+        {
+            RCC_SceneManager.SetBehavior(BehaviourType);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
